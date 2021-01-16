c349bd04e50523e79a4740f806fcfbf749020ba0
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-01-15 22:59:12 -0300

    added project

---
+++ src/main/java/com/raimiyashiro/restTemp2/RestTemp2Application.java
@@ -0,0 +1,13 @@
+package com.raimiyashiro.restTemp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/raimiyashiro/restTemp2/RestTemp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimiyashiro.restTemp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
